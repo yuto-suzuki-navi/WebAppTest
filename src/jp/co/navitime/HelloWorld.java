88a8f277aaaa9eea952728654f3b6d09ccf61c1e
Yuto Suzuki <yuto.suzuki.business@gmail.com>
2019-05-11 15:43:33 +0900

    Initial Commit

---
+++ src/jp/co/navitime/HelloWorld.java
@@ -0,0 +1,24 @@
+package jp.co.navitime;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+	response.setContentType("text/html");
+	PrintWriter out = response.getWriter();
+	out.println("<html>");
+	out.println("<head>");
+	out.println("<title>Hello World!</title>");
+	out.println("</head>");
+	out.println("<body>");
+	out.println("<h1>Hello World!</h1>");
+	out.println("</body>");
+	out.println("</html>");
+	}
+}
